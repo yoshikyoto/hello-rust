8906655813a021bbe4fa8811a3f565c74232c755
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2021-08-04 05:39:50 +0900

    input/output

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+use std::io;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    println!("Guess the number");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    println!("You guessed: {}", guess)
 }
